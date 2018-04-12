e3d13218908dada1f13d36430338f320280841a5
Sachin <sachinwork91@gmail.com>
2018-04-12 19:35:48 -0400

    Added a new File To Add two Numbers using single OnclickListener as Interface

---
+++ app/src/main/java/com/example/sachin/kotlintutorials/AddTwoNumbers.kt
@@ -0,0 +1,68 @@
+package com.example.sachin.kotlintutorials
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_add_two_numbers.*
+
+//This activity demonstrates the use of a common Onclick Listener by the two buttons
+
+
+class AddTwoNumbers : AppCompatActivity()  , View.OnClickListener{
+
+
+
+    //  EditText editText;
+
+    lateinit var num1et: EditText
+    lateinit var num2et: EditText
+    lateinit var resultText: TextView
+    lateinit var addbutton: Button
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_two_numbers)
+
+        num1et = findViewById(R.id.num1)
+        num2et = findViewById(R.id.num2)
+        resultText = findViewById(R.id.res)
+        addbutton = findViewById(R.id.addbutton)
+
+
+        addbutton.setOnClickListener(this)
+        subbutton.setOnClickListener(this)
+    }
+
+
+    override fun onClick(view: View?) {
+
+        when(view?.id){
+
+            R.id.addbutton -> {
+                val num1 = num1et.text.toString().toInt()
+                val num2 = num2et.text.toString().toInt()
+
+                resultText.setText( (num1+num2).toString())
+            }
+
+            R.id.subbutton -> {
+                val num1 = num1et.text.toString().toInt()
+                val num2 = num2et.text.toString().toInt()
+
+                resultText.setText((num1-num2).toString() )
+            }
+
+
+
+
+        }
+
+
+    }
+
+}
